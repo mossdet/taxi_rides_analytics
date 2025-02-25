bf1ae4cedd143b8aaa2dc47f937cbbfbd4095be2
Dan Lap <dalapiz@proton.me>
2025-02-25 09:50:03 +0000

    compute duration p90 for each pickup_location->dropoff_location pair

--- models/core/fct_fhv_monthly_zone_traveltime_p90.sql
+++ models/core/fct_fhv_monthly_zone_traveltime_p90.sql
@@ -1,6 +1,26 @@
+with dropoff_zones_prctl as (
+    select
+        pickup_zone,
+        dropoff_zone,
+        year,
+        month,
+        TIMESTAMP_DIFF(dropoff_datetime, pickup_datetime, SECOND) as trip_duration,
+        ((percentile_cont(TIMESTAMP_DIFF(dropoff_datetime, pickup_datetime, SECOND), 0.90) OVER 
+        (PARTITION BY year, month, pickup_zone, dropoff_zone))) as p90,
+    from 
+        {{ ref('dim_fhv_trips') }}
+)
 select
-    *,
-    extract(SECOND from dropoff_datetime - pickup_datetime) as trip_duration,
-    (percentile_cont(extract(SECOND from dropoff_datetime - pickup_datetime) , 0.97) OVER 
-        (PARTITION BY year, month, pickup_locationid, dropoff_locationid)) as p97,
-from {{ ref('dim_fhv_trips') }}
+    pickup_zone,
+    dropoff_zone,
+    avg(p90) as p90
+from dropoff_zones_prctl
+where
+    --pickup_zone in ('Newark Airport', 'SoHo', 'Yorkville') and
+    pickup_zone in ('Newark Airport') and
+    --pickup_zone in ('SoHo') and
+    --pickup_zone in ('Yorkville East' ) and
+    year = 2019 and
+    month = 11
+group by pickup_zone, dropoff_zone
+order by p90 desc
