85e5e3e9ac0a5880ff843445feb38569a6d01caf
Dan Lap <dalapiz@proton.me>
2025-02-25 07:51:48 +0000

    add stg__fhv_tripdata and dim_fhv_trips

---
+++ models/staging/stg__fhv_tripdata.sql
@@ -0,0 +1,39 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+with tripdata as 
+(
+    select *,
+    row_number() over(partition by dispatching_base_num, unique_row_id, pickup_datetime) as rn,
+    cast(REPLACE(dispatching_base_num, 'B', '') as int) as dbit_int
+    from {{ source('staging','fhv_tripdata') }}
+    where dispatching_base_num is not null
+)
+select
+    -- identifiers
+    cast({{ dbt_utils.generate_surrogate_key(['dbit_int', 'pickup_datetime', 'dropoff_datetime']) }} as string) as tripid,
+    cast(dispatching_base_num as string) as dispatch_base_id,
+    {{ dbt.safe_cast("PULocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("DOLocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,
+    
+    -- -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- -- trip info
+    {{ dbt.safe_cast("SR_Flag", api.Column.translate_type("integer")) }} as sr_flag,
+    cast(Affiliated_base_number as string) as affiliated_base_number
+from tripdata
+where rn = 1
+
+
+-- Dev limit
+-- dbt build --select <model.sql> --vars '{'is_test_run: false}'
+{% if var('is_test_run', default=false) %}
+
+  limit 100
+
+{% endif %}
