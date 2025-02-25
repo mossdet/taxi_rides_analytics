f59ab7579c086c3e002b49358e8513307e7f5ef2
Dan Lap <dalapiz@proton.me>
2025-02-25 09:12:23 +0000

    correct joining of trips and zones tables

--- models/core/dim_fhv_trips.sql
+++ models/core/dim_fhv_trips.sql
@@ -23,5 +23,22 @@ dim_zones as (
     select * from {{ ref('dim_zones') }}
     where borough != 'Unknown'
 )
-select * from fhv_tripdata 
-inner join dim_zones on fhv_tripdata.pickup_locationid = dim_zones.locationid
+select
+    service_type,
+    year,
+    month,
+    tripid, 
+    dispatch_base_id, 
+    pickup_locationid,
+    dropoff_locationid, 
+    pickup_datetime, 
+    dropoff_datetime,
+    sr_flag, 
+    affiliated_base_number,
+    pickup_zone.borough as pickup_borough, 
+    pickup_zone.zone as pickup_zone, 
+    dropoff_zone.borough as dropoff_borough, 
+    dropoff_zone.zone as dropoff_zone
+from fhv_tripdata 
+inner join dim_zones as pickup_zone on fhv_tripdata.pickup_locationid = pickup_zone.locationid
+inner join dim_zones as dropoff_zone on fhv_tripdata.dropoff_locationid = dropoff_zone.locationid
--- models/core/fct_fhv_monthly_zone_traveltime_p90.sql
+++ models/core/fct_fhv_monthly_zone_traveltime_p90.sql
@@ -1,4 +1,5 @@
 select
+    *,
     extract(SECOND from dropoff_datetime - pickup_datetime) as trip_duration,
     (percentile_cont(extract(SECOND from dropoff_datetime - pickup_datetime) , 0.97) OVER 
         (PARTITION BY year, month, pickup_locationid, dropoff_locationid)) as p97,
